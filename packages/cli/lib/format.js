452cb5aac010c5bef73df674285186d4d6ff7ad0
Pranay Prakash <pranay.gp@gmail.com>
2020-08-22 18:28:19 -0700

    add cli commands, allow inline RuleExpr and format all the examples

--- packages/cli/lib/format.js
+++ packages/cli/lib/format.js
@@ -10,21 +10,52 @@ const parser = require("@browselang/parser");
 
 exports.command = "format <script>";
 exports.handler = "Format a browse script";
-exports.builder = () => {};
+exports.builder = (yargs) => {
+  yargs
+    .option("check", {
+      type: "boolean",
+      alias: "c",
+      default: false,
+      description:
+        "Check if a file has been formatted correctly. Exit with a non-zero code if the file is not formatted",
+    })
+    .option("write", {
+      type: "boolean",
+      alias: "w",
+      default: false,
+      description:
+        "Write the formatted output back to the file, overwriting its contents",
+    });
+};
 
 exports.handler = async (argv) => {
-  const { script } = argv;
+  const { script, write, check } = argv;
+
+  if (write && check) {
+    console.error(
+      "The --write and --check options cannot be used simultaneously"
+    );
+  }
 
   const document = path.resolve(process.cwd(), script);
   try {
-    const code = fs.readFileSync(document, "utf8").trim();
+    const code = fs.readFileSync(document, "utf8");
 
     const data = formatter.formatWithCursor(code, {
       parser: "browse",
       plugins: [],
     });
 
-    console.log(data.formatted);
+    if (check) {
+      if (data.formatted !== code) {
+        console.error("The code is not formatted correctly");
+        process.exit(1);
+      }
+    } else if (write) {
+      fs.writeFileSync(document, data.formatted);
+    } else {
+      console.log(data.formatted);
+    }
   } catch (err) {
     process.stderr.write(
       stringifyError(err, {
