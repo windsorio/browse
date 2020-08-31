e3b69c79a55dc0c269c8b0c34dbebbf576c06b12
Pranay Prakash <pranay.gp@gmail.com>
2020-08-31 13:50:31 -0700

    add publish script

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Pranay needs to run this. Let him know if you want to publish a new version
+
+PKG_DIRS=$(ls packages)
+
+# Bump all versions
+for pkg in $PKG_DIRS; do
+  rm -rf .yarn/versions
+  cd "packages/$pkg"
+  yarn version patch
+  cd ../../
+done
+rm -rf .yarn/versions
+
+# Publish each one
+for pkg in $PKG_DIRS; do
+  cd "packages/$pkg"
+  yarn npm publish --access public
+  cd ../../
+done
+
+# Update "format" lib in vscode and publish
+cd vscode
+yarn add @browselang/format
+vsce publish patch
+cd ..
