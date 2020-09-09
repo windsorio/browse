d0adbbb066604373f313ac1bd5e92daa52ba33e3
Andrew <atfaust2@illinois.edu>
2020-09-08 23:36:24 -0400

    Leading comments works

--- packages/docs/parsers/browse.js
+++ packages/docs/parsers/browse.js
@@ -1,4 +1,23 @@
 const parser = require("@browselang/parser");
+const util = require("util");
+
+const show = (obj) =>
+  console.log(util.inspect(obj, false, null, true /* enable colors */));
+
+const getChildren = (type) =>
+  ({
+    Program: ["rules"],
+    Rule: ["fn", "args"],
+    RuleSet: ["rules"],
+    Paren: ["expr"],
+    UnaryExpr: ["expr"],
+    BinExpr: ["left", "right"],
+    RuleExpr: ["expr"],
+    InitRule: ["module", "name"],
+    Word: [],
+    Literal: [],
+    Ident: [],
+  }[type]);
 
 const parseComments = (ast) => {
   const commentBlocks = ast.comments.reduce((p, c) => {
@@ -15,14 +34,110 @@ const parseComments = (ast) => {
     return p;
   }, []);
 
-  console.log(commentBlocks);
+  return commentBlocks;
+};
+
+const dfsTraverse = (node, fn) => {
+  fn(node);
+  const children = getChildren(node.type);
+  if (children) {
+    children.forEach((child) => {
+      if (node[child]) {
+        if (Array.isArray(node[child])) {
+          node[child].map((child) => dfsTraverse(child, fn));
+        } else {
+          dfsTraverse(node[child], fn);
+        }
+      } else if (node[child] === undefined) {
+        //TODO: module should not be undefined. Null or empty object is better
+        if (child !== "module") {
+          show(node);
+          throw new Error(
+            `Node did not have the ${child} child indicated by 'getChildren'`
+          );
+        }
+      }
+    });
+  } else if (child === undefined) {
+    show(node);
+    throw new Error(`Unknown Node type for getChildren ${node.type}`);
+  }
+};
+
+const bfsTraverse = (node, fn) => {
+  const queue = [];
+  queue.push(node);
+  while (queue.length) {
+    const curr = queue.shift();
+    fn(curr);
+    const children = getChildren(curr.type);
+
+    if (children) {
+      children.forEach((child) => {
+        if (curr[child]) {
+          if (Array.isArray(curr[child])) {
+            queue.push(...curr[child]);
+          } else {
+            queue.push(curr[child]);
+          }
+        } else if (curr[child] === undefined) {
+          //TODO: module should not be undefined. Null or empty object is better
+          if (child !== "module") {
+            show(curr);
+            throw new Error(
+              `Node did not have the child ${child}indicated by 'getChildren'`
+            );
+          }
+        }
+      });
+    } else if (children === undefined) {
+      show(curr);
+      throw new Error(`Unknown Node type for getChildren ${curr.type}`);
+    }
+  }
+};
+
+const assignLeadingComment = (ast, comments) => {
+  /*
+   * We're going to walk through the AST and look for leading from the list
+   */
+
+  const sortedTree = [];
+
+  let sortedTreeIdx = 0;
+
+  //Bfs walks us through the tree in a sorted manner
+  bfsTraverse(ast, (node) => sortedTree.push(node));
+
+  for (i in comments) {
+    const comment = comments[i];
+    const commentEnd = comment.source.endIdx;
+    while (sortedTreeIdx < sortedTree.length) {
+      const node = sortedTree[sortedTreeIdx];
+      /*      console.log(`Comment ${comment.source.startIdx}::${comment.source.endIdx}`);
+      console.log(`Node ${node.source.startIdx}::${node.source.endIdx}`);
+      console.log("Should discard?", node.source.startIdx < commentEnd); */
+      //If the node starts before the comment ends, we don't want it
+      if (node.source.startIdx < commentEnd) {
+        sortedTreeIdx++;
+      }
+      //Since everything is sorted, the first node we find is the one the comment belongs to
+      else {
+        if (node.comments) node.leadingComments.push(comment);
+        else node.leadingComments = [comment];
+        break;
+      }
+    }
+  }
 };
 
 module.exports = (code, fileName) => {
   const rtn = {};
 
   const ast = parser.parse(code);
-  parseComments(ast);
 
+  assignLeadingComment(ast, parseComments(ast));
+
+  dfsTraverse(ast, (node) => node.leadingComments && console.log(node));
   return rtn;
 };
