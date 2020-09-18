dcbd5b04590e8e2e4c4f8548923eeec5eb5ec01e
Pranay Prakash <pranay.gp@gmail.com>
2020-09-18 16:56:05 -0700

    add sys stdlib

--- packages/core/stdlib/sys/native.js
+++ packages/core/stdlib/sys/native.js
@@ -8,6 +8,8 @@ exports.browse = (parent) => {
 
   sysScope.rules = {
     argv: (_) => (_) => (n) => process.argv[n],
+    env: (_) => (_) => (key) => process.env[key],
+    exit: (_) => (_) => (code) => process.exit(Number(code)),
   };
 
   return sysScope;
