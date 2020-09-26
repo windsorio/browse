8f085656cccdd4e1daa6fef6e3fabf1cad5c0700
chinchaun <jjdelannoy@hotmail.com>
2020-09-26 18:52:40 -0300

    Added the evaluation expression enum

---
+++ packages/core/lib/enums/EvalExprTypeEnum.ts
@@ -0,0 +1,11 @@
+enum EvalExprTypeEnum {
+  Paren = "Paren",
+  Literal = "Literal",
+  Ident = "Ident",
+  RuleSet = "RuleSet",
+  RuleExpr = "RuleExpr",
+  UnaryExpr = "UnaryExpr",
+  BinExpr = "BinExpr"
+}
+
+export default EvalExprTypeEnum;
